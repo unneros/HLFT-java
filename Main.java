346705a8c19ee4740fccc47597311e29e8a0b59d
Phuoc Bui <xphuocbui@gmail.com>
2020-02-26 17:53:57 +0100

    phuoc

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
+
 public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
@@ -11,8 +13,23 @@ public class Main {
         int a = Integer.parseInt(abc[0]);
         int b = Integer.parseInt(abc[1]);
         int c = Integer.parseInt(abc[2]);
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println(-c);
+            }
+            else {
+                System.out.println(-c/b);
+            }
+        }
+        else {
+            if (b*b - 4*a*c < 0) {
+                System.out.println("vietnam");
+            }
+            else {
+                System.out.println((-b+sqrt(b*b - 4*a*c))/(2*a));
+                System.out.println((-b-sqrt(b*b - 4*a*c))/(2*a));
+            }
+        }
     }
 }
